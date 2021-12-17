fae8337b82ead945832353cf31e15ee8b83fcb31
narseneva <86502945+narseneva@users.noreply.github.com>
2021-12-16 21:58:22 -0500

    fitness12

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Natalia Douglas
+    File Name: script.js
+    Date: 12/14/2021
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+  $('div.container.welcome').height($(window).height());
+});
